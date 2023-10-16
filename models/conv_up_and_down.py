cfccbaa82a2e7720d5f2fb295e62ff4b7d1c61e2
kotlyar-shapirov <kotlyar.shapirov@gmail.com>
2023-10-16 16:53:58 +0300

    Added NIPS architecture, tested in Airat jupyter

--- models/conv_up_and_down.py
+++ models/conv_up_and_down.py
@@ -3,7 +3,7 @@ from torch import nn
 
 # Squeezing convolution
 class DownsampleBlock(nn.Module):
-    def __init__(self, in_features, out_features, nonlinearity=nn.ELU()):
+    def __init__(self, in_features, out_features, nonlinearity=nn.ReLU()):
         super().__init__()
         self.conv = nn.Conv2d(in_features, out_features,
                               kernel_size=4, stride=2, padding=1, dilation=1)
@@ -16,10 +16,10 @@ class DownsampleBlock(nn.Module):
 
 # Upscaling inverse convolution
 class UpsampleBlock(nn.Module):
-    def __init__(self, in_features, out_features, scale_factor=2, nonlinearity=nn.ELU()):
+    def __init__(self, in_features, out_features, scale_factor=2, nonlinearity=nn.ReLU(), kernel_size=4, stride=2, padding=1):
         super().__init__()
         self.upsample = nn.ConvTranspose2d(in_features, out_features,
-                                           kernel_size=4, output_padding=0, padding=1, stride=2)
+                                           kernel_size=kernel_size, stride=stride, padding=padding, output_padding=0,)
         self.nonlinearity = nonlinearity
         
     def forward(self, x):
