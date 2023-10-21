8f3c50717199a89062027133eca985140c1f1159
kotlyar-shapirov <kotlyar.shapirov@gmail.com>
2023-10-21 13:16:39 +0300

    run_train and run_script minor changes
    rebasing the branch for wasser

--- utils/loss.py
+++ utils/loss.py
@@ -20,11 +20,12 @@ def wasser_loss(factors_probability):
     B, C, N_bins = factors_probability.shape
     factors_probability = torch.nn.Softmax(dim=-1)(factors_probability)
     factors_cdf = torch.cumsum(factors_probability, dim=-1)
-    # diff_abs_cdf = torch.abs(factors_cdf - torch.arange(1,factors_cdf.shape[-1]+1).to(device)/factors_cdf.shape[-1])
-    # integral_diff = torch.sum(diff_abs_cdf*1/diff_abs_cdf.shape[-1], dim=-1)
-    # wasser_mean_loss = torch.mean(torch.sum(torch.sqrt(integral_diff**2), dim=-1))/(factors_cdf.shape[1])
-
     diff_abs_cdf = torch.abs(factors_cdf - torch.arange(1,N_bins+1).to(device)/N_bins)
     integral_diff = torch.sum(diff_abs_cdf/N_bins, dim=-1)
     wasser_mean_loss = torch.mean(torch.sum(torch.sqrt(integral_diff**2), dim=-1))/C
     return wasser_mean_loss
+
+    # diff_abs_cdf = torch.abs(factors_cdf - torch.arange(1,N_bins+1).to(device)/N_bins)
+    # integral_diff = torch.sum(diff_abs_cdf/N_bins, dim=-1)
+    # wasser_mean_loss = torch.mean(torch.sum(torch.sqrt(integral_diff**2), dim=-1))/C
+    # return wasser_mean_loss
